dfaae90b19933bffaae68e2e288adab16e0a2613
bz44 <baobao.zhang@yale.edu>
2015-12-27 07:40:49 -0500

    proof of concept

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# randomize_fit
-Randomly Assigned Exercises to Email Inbox
+# Randomize Fit
+
+Randomly Assigned Exercises Send to Your Email
+
+This is a proof of concept for Randomize Fit, an app that alerts you daily to encourage you to exercise. You will be randomly assigned a certain number of minutes to exercise for that day. The Python script `email_script.py` will send you and/or other recipients an email alert.
+
+# Directions for use
+
+1. Fill in the necessary email information in the Python script.
+
+2. Schedule the script to run at a set time daily so it can blast out some emails. You can use [crontab](https://docs.oracle.com/cd/E23824_01/html/821-1451/sysrescron-24589.html).
+
+If you want to blast out the emails at 6 a.m. each day, add this to the end of your crontab file:
+
+```
+0 6 * * * python /whatever_directory/email_script.py
+```
---
+++ python/email_script.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import smtplib
+import random
+ 
+# set up server info
+server = smtplib.SMTP('STMP ADDRESS', PORT_NUMBER)
+server.starttls()
+server.login("SENDER EMAIL ADDRESS", "EMAIL PASSWORD")
+
+# randomize minutes of exercise
+minutes_excercise = random.choice([0, 15, 30, 45, 60])
+
+# compose message
+if minutes_excercise > 0:
+	subject_text = "Exercise for " + str(minutes_excercise) + " Minutes Today!"
+	msg_text = "Good morning! Please excercise for " + str(minutes_excercise) + \
+	" minutes today. Thank you for using Randomize Fit."
+else:
+	subject_text = "Take the Day Off!"
+	msg_text = "Good morning! You don't have to exercise today." \
+	" Enjoy your day off! Thank you for using Randomize Fit."
+
+message = 'Subject: %s\n\n%s' % (subject_text, msg_text)
+
+recipients = ["EMAIL OF RECIPIENTS"]
+
+# send email
+server.sendmail("SENDER EMAIL ADDRESS", recipients, message)
+server.quit()
